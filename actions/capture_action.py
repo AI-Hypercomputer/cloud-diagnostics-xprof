634102872115590ec90b1511f8a09784ee40fba0
Victor Vector <victorsvector@google.com>
2025-03-19 18:00:07 +0000

    Upload profile capture script to workload VM
    - Script to capture profile must exist in workload VM to capture profile
      in log directory. Update also uploads script to workload VM before
      capturing profile.
    - Capture profile for only one workload VM.
    
    PiperOrigin-RevId: 738464433

--- actions/capture_action.py
+++ actions/capture_action.py
@@ -22,7 +22,6 @@ is that this can be used to capture a profile from an instance using the
 
 import argparse
 from collections.abc import Mapping, Sequence
-from typing import cast
 from actions import action
 
 
@@ -57,21 +56,22 @@ class Capture(action.Command):
         required=True,
         help='The log directory to capture a profile to.',
     )
-    # zone is optional.
+    # zone is required.
     capture_parser.add_argument(
         '--zone',
         '-z',
         metavar='ZONE_NAME',
+        required=True,
         help='The GCP zone to the instance in for the profile capture.',
     )
-    # hosts must be specified and can be multiple.
+    # hosts must be specified
     capture_parser.add_argument(
         '--hosts',
         '-n',
         metavar='HOST_NAME',
-        required=True,
         nargs='+',
-        help='The host names to capture a profile from.',
+        required=True,
+        help='The host name to capture a profile from.',
     )
     # port is optional.
     capture_parser.add_argument(
@@ -121,30 +121,120 @@ class Capture(action.Command):
   ) -> Sequence[str]:
 
     command = []
-    for host in args.hosts:
-      profile_log_location = (
-          f'{args.log_directory}/{args.experiment_name}/{args.run_name}'
-      )
-      profile_script_command = (
-          'python3 capture_profile.py'
-          f' --service_addr "localhost:{args.port}"'
-          f' --logdir {profile_log_location}'
-          f' --duration_ms {args.duration}'
-      )
-      profile_command = [
-          self.GCLOUD_COMMAND,
-          'compute',
-          'tpus',
-          'tpu-vm',
-          'ssh',
-          host,
-          '--zone',
-          args.zone,
-          '--worker=all',
-          '--command',
-          f'{profile_script_command}',
-      ]
-      command.extend(profile_command)
+
+    # Build the profile command
+    profile_log_location = (
+        f'{args.log_directory}/{args.experiment_name}/{args.run_name}'
+    )
+    profile_script_command = (
+        'python3 capture_profile.py'
+        f' --service_addr "localhost:{args.port}"'
+        f' --logdir {profile_log_location}'
+        f' --duration_ms {args.duration}'
+    )
+    profile_command = [
+        self.GCLOUD_COMMAND,
+        'compute',
+        'tpus',
+        'tpu-vm',
+        'ssh',
+        args.host,
+        '--zone',
+        args.zone,
+        '--worker=all',
+        '--command',
+        f'{profile_script_command}',
+    ]
+    command.extend(profile_command)
 
     return command
 
+  def _profile_single_host(
+      self,
+      host: str,
+      zone: str,
+      args: argparse.Namespace,
+      extra_args: Mapping[str, str] | None = None,
+      verbose: bool = False,
+  ) -> str:
+    """Runs the profile script on a single host."""
+    stdout_all = ''
+
+    upload_script_command = (
+        'wget https://raw.githubusercontent.com/pytorch/xla/master/'
+        'scripts/capture_profile.py'
+    )
+
+    if verbose:
+      print(f'Running profile capture on {host} host...')
+
+    # Upload the capture profile script to host.
+    if verbose:
+      print('Uploading profile script to host...')
+    upload_profile_script_command = [
+        self.GCLOUD_COMMAND,
+        'compute',
+        'tpus',
+        'tpu-vm',
+        'ssh',
+        host,
+        '--zone',
+        zone,
+        '--worker=all',
+        '--command',
+        f'{upload_script_command}',
+    ]
+    upload_profile_script_stdout = self._run_command(
+        command=upload_profile_script_command,
+        verbose=verbose,
+    )
+    stdout_all += upload_profile_script_stdout
+
+    # Run the profile script on host.
+    if verbose:
+      print(f'Running profile script on single host {host}...')
+
+    # Include a host-specific argument for the host name.
+    single_host_args = argparse.Namespace(**vars(args))
+    single_host_args.host = host
+    single_host_profile_command = self._build_command(
+        args=single_host_args,
+        extra_args=extra_args,
+        verbose=verbose,
+    )
+    stdout_single_host_profile = self._run_command(
+        command=single_host_profile_command,
+        verbose=verbose,
+    )
+    stdout_all += stdout_single_host_profile
+
+    return stdout_all
+
+  def run(
+      self,
+      args: argparse.Namespace,
+      extra_args: Mapping[str, str] | None = None,
+      verbose: bool = False,
+  ) -> str:
+
+    stdout_all_hosts: list[str] = []
+
+    if verbose:
+      print(f'Running profile capture on {len(args.hosts)} hosts...')
+
+    for host in args.hosts:
+      # Run the profile script on a single host.
+      try:
+        single_host_stdout = self._profile_single_host(
+            host=host,
+            zone=args.zone,
+            args=args,
+            extra_args=extra_args,
+            verbose=verbose,
+        )
+        stdout_all_hosts.append(single_host_stdout)
+      # Catch issues with profiling the host.
+      except Exception as e:  # pylint: disable=broad-except
+        print(f'Failed to profile host {host}: {e}')
+
+    return '\n'.join(stdout_all_hosts)
